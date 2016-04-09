3bcf79ba60a8f9d0001ee96ba7b935f55f2874f4
MuffinUprising <casey.holmgren@gmail.com>
2016-04-09 18:13:01 -0500

    added urls and first html template

---
+++ cardsearch/urls.py
@@ -0,0 +1,8 @@
+__author__ = 'casey'
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.card_list, name='card_list'),
+]
--- cardsearch/views.py
+++ cardsearch/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def card_list(request):
+    return render(request, 'cardsearch/card_list.html', {})
