c4e1d451734a209502206e5af755359571ce704d
MuffinUprising <casey.holmgren@gmail.com>
2016-04-09 19:13:22 -0500

    Changed option from 'blank=True' to 'null=True'

--- cardsearch/models.py
+++ cardsearch/models.py
@@ -22,25 +22,25 @@ class Card(models.Model):
     # Epic dust:[400,1600,100,400]
     # Legendary dust:[1600,3200,400,1600]
 
-    dust_cost = models.IntegerField(blank=True)
-    dust_value = models.IntegerField(blank=True)
-    golden_dust_cost = models.IntegerField(blank=True)
-    golden_dust_value = models.IntegerField(blank=True)
+    dust_cost = models.IntegerField(null=True)
+    dust_value = models.IntegerField(null=True)
+    golden_dust_cost = models.IntegerField(null=True)
+    golden_dust_value = models.IntegerField(null=True)
 
     # Minion and Weapon
-    attack = models.IntegerField(blank=True)
+    attack = models.IntegerField(null=True)
     # Minion only
-    health = models.IntegerField(blank=True)
-    mechanics = models.TextField(blank=True)
+    health = models.IntegerField(null=True)
+    mechanics = models.TextField(null=True)
 
     # Not all cards have card text
-    card_text = models.TextField(blank=True)
+    card_text = models.TextField(null=True)
 
     # Only available for class-specific cards
-    player_class = models.CharField(max_length=12, blank=True)
+    player_class = models.CharField(max_length=12, null=True)
 
     # Weapon only (Note: weapon also has attack value)
-    weapon_durability = models.IntegerField(blank=True)
+    weapon_durability = models.IntegerField(null=True)
 
     def __str__(self):
         return self.card_name
